5935fcbbfcd3ecb728543db9141378bfdf0d2e14
tomocy <tomocy.dev@gmail.com>
2020-04-03 21:26:46 +0900

    add viaHTTP
    viaHTTP implements Loader.

--- memstats.go
+++ memstats.go
@@ -2,9 +2,38 @@ package memstats
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"runtime"
 )
 
 type Loader interface {
 	Load(context.Context) (*runtime.MemStats, error)
+}
+
+type viaHTTP struct {
+	*http.Client
+	url string
+}
+
+func (l *viaHTTP) Load(ctx context.Context) (*runtime.MemStats, error) {
+	resp, err := l.Get(l.url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get memory statics: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get memory statics successfully: %w", fmt.Errorf(resp.Status))
+	}
+
+	var loaded struct {
+		Stats *runtime.MemStats `json:"memstats"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&loaded); err != nil {
+		return nil, fmt.Errorf("failed to decode body: %w", err)
+	}
+
+	return loaded.Stats, nil
 }
